ab134702289d3be4cd7468dafaba4e92dc64e9a2
Aashir Rasadh <159936314+AashirRasadh@users.noreply.github.com>
2025-12-19 10:35:47 +0530

    Add web_server_get_handle function declaration
    Added function declaration for web_server_get_handle.

--- components/web_server/include/web_server.h
+++ components/web_server/include/web_server.h
@@ -4,17 +4,35 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+// #pragma once
+
+// #include <esp_err.h>
+
+// #ifdef __cplusplus
+// extern "C"
+// {
+// #endif
+
+// esp_err_t StartWebServer(void);
+
+// #ifdef __cplusplus
+// }
+// #endif
+
 #pragma once
 
 #include <esp_err.h>
+#include "esp_http_server.h"     // <-- add this
 
 #ifdef __cplusplus
-extern "C"
-{
+extern "C" {
 #endif
 
 esp_err_t StartWebServer(void);
 
+httpd_handle_t web_server_get_handle(void);   // <-- add this
+
 #ifdef __cplusplus
 }
 #endif
+ 
